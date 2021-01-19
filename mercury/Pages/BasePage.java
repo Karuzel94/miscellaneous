11f9f9404a67c55245a15edc5d9740126751c7f4
Piotr_K <pitex1401@gmail.com>
2021-01-19 12:48:13 +0100

    Added click() method

--- mercury/Pages/BasePage.java
+++ mercury/Pages/BasePage.java
@@ -3,14 +3,23 @@ package misc_exercises.mercury.Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
+    private int time = 5;
     protected WebDriver driver;
     protected WebElement element;
+    protected WebDriverWait w;
 
     public BasePage(WebDriver driver){
         this.driver = driver;
+        this.w = new WebDriverWait(this.driver, time);
+    }
+
+    public void click(WebElement element){
+        w.until(ExpectedConditions.visibilityOf(element));
+        w.until(ExpectedConditions.elementToBeClickable(element)).click();
     }
 
 }
